18a3216855d9170366e7626b013ce7cbcdaf6dfa
Fred Sobel <fred.sobel@gmail.com>
2024-09-21 15:16:58 -0500

    Reorganize folders

---
+++ vue-components-module/components/vue-detail-component.js
@@ -0,0 +1,30 @@
+const template = `
+    <div v-if="context.selectedItem">
+        <div class="col-12">
+            <label for="Name" class="form-label">Name: {{context.selectedItem.name}}</label>
+        </div>
+        <div class="col-12">
+            <label for="Position" class="form-label">Position: {{ context.selectedItem.position }}</label>
+        </div>
+        <div class="col-12">
+            <label for="Office" class="form-label">Office: {{ context.selectedItem.office }}</label>
+        </div>
+        <div class="col-12">
+            <label for="Office" class="form-label">Age: {{ context.selectedItem.age }}</label>
+        </div>
+        <div class="col-12">
+            <img style="width: 100%;" :src="context.selectedItem.image">
+        </div>
+    </div>
+`;
+
+
+export default {
+    props: ["context"],
+    data() {
+        return {}
+    },
+    template: template,
+    computed: {},
+    methods: {},
+}
---
+++ vue-components-module/index.js
@@ -0,0 +1,45 @@
+import List from './components/vue-list-component.js'
+import Edit from './components/vue-edit-component.js'
+import Detail from './components/vue-detail-component.js'
+
+const { createApp } = Vue;
+        
+const app = createApp({
+     components: {
+         'vue-list-component': List,
+         'vue-edit-component': Edit,
+         'vue-detail-component': Detail
+       },
+    data() {
+        return {
+            context: {
+                headers: [],
+                items: [],
+                selectedItem: null,
+            }
+        }
+    },
+    methods: {
+        async getItems() {
+            const items = [
+                { name: "Oliver", position: "Accountant", office: "Living Room", age: 8, image: "http://placekitten.com/210/200" },
+                { name: "Milo", position: "Chief Executive Officer (CEO)", office: "Monica's room", age: 7, image: "http://placekitten.com/g/200/200" },
+                { name: "Cedric", position: "Senior Javascript Developer", office: "Kitchen", age: 5, image: "http://placekitten.com/202/203" },
+                { name: "Sprinkles", position: "Sleeping", office: "Basement", age: 6, image: "http://placekitten.com/g/203/200" },
+            ];
+            const headers = [
+                { key: "name", value: "Name" },
+                { key: "position", value: "Position" },
+                { key: "office", value: "Office" },
+                { key: "age", value: "Age" },
+            ];
+            this.context.headers = headers;
+            this.context.items = items;
+            this.context.selectedItem = this.context.items[0];
+            this.context.searchString = "";
+        }
+    },
+    mounted() {
+        this.getItems();
+    }
+}).mount('#app')
