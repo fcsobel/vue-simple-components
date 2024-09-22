e85e43bafe374c9e25e9828d63fb1d7c88aa9dee
Fred Sobel <fred.sobel@gmail.com>
2024-09-22 17:03:56 -0500

    Switch to use data

--- vue-components-dynamic/index.js
+++ vue-components-dynamic/index.js
@@ -21,12 +21,7 @@ const app = createApp({
     },
     methods: {
         async getItems() {
-            const items = [
-                { name: "Oliver", position: "Accountant", office: "Living Room", age: 8, image: "http://placecats.com/210/200" },
-                { name: "Milo", position: "Chief Executive Officer (CEO)", office: "Monica's room", age: 7, image: "http://placecats.com/g/200/200" },
-                { name: "Cedric", position: "Senior Javascript Developer", office: "Kitchen", age: 5, image: "http://placecats.com/202/203" },
-                { name: "Sprinkles", position: "Sleeping", office: "Basement", age: 6, image: "http://placecats.com/g/203/200" },
-            ];
+
             const headers = [
                 { key: "name", value: "Name" },
                 { key: "position", value: "Position" },
@@ -34,9 +29,14 @@ const app = createApp({
                 { key: "age", value: "Age" },
             ];
             this.context.headers = headers;
-            this.context.items = items;
-            this.context.selectedItem = this.context.items[0];
             this.context.searchString = "";
+
+            fetch('/data/users.json')
+                .then(res => res.json())
+                .then(items => {
+                    this.context.items = items;
+                    this.context.selectedItem = this.context.items[0];
+                });
         }
     },
     mounted() {
